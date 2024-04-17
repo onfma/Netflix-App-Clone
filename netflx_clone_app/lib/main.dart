ec6e47b4e06085d60b06b384cd4256151a916a5c
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2024-04-17 21:17:18 +0300

    components outline homepage

--- netflx_clone_app/lib/main.dart
+++ netflx_clone_app/lib/main.dart
@@ -27,8 +27,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
 
-  // @override
-  // NavBar createState() => NavBar();
   @override
   // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
@@ -37,31 +35,33 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          NavBar(), // Add the NavBar widget
-          Header(), // Add the Header widget
-        ],
+    return Scaffold(
+      body: SingleChildScrollView( // Add this
+        child: Column(
+          children: [
+            const NavBar(), // Add the NavBar widget
+            const Header(), // Add the Header widget
+            Container(
+              margin: const EdgeInsets.all(30), // Set the margin you want
+              child: const MovieSection(title: 'Top 10 - Romania'),
+            ), 
+            Container(
+              margin: const EdgeInsets.all(30), // Set the margin you want
+              child: const MovieSection(title: 'Popular Series'),
+            ),
+            Container(
+              margin: const EdgeInsets.all(30), // Set the margin you want
+              child: const MovieSection(title: 'Popular Movies'),
+            ),
+            Container(
+              margin: const EdgeInsets.all(30), // Set the margin you want
+              child: const SpecialSection(title: 'Netflix Originals'),
+            ), 
+          ],
+        ),
       ),
     );
   }
-}
-
-class Header extends StatelessWidget{
-  const Header({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-      return SizedBox(
-        height: 500, // Set the height to 200
-        width: MediaQuery.of(context).size.width, // Set the width to the screen width
-        child: Image.network(
-          'assets/tdwp-header.jpg',
-          fit: BoxFit.fitWidth, // Use BoxFit.cover to scale and crop the image
-        ),
-      );
-    }
 }
 
 class NavBar extends StatelessWidget {
@@ -120,6 +120,108 @@ class NavBar extends StatelessWidget {
           onPressed: () {
             // Add your action here
           },
+        ),
+      ],
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('assets/tdwp-header.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(100.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'The Devil Wears \n Prada',
+                style: TextStyle(
+                  fontSize: 60, 
+                  color: Color.fromRGBO(255, 195, 195, 1),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Watch Now'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MovieSection extends StatelessWidget {
+  const MovieSection({super.key, required this.title});
+  final String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 40, 
+            color: Color.fromRGBO(255, 195, 195, 1),
+          ),
+        ),
+        SizedBox(
+          height: 150,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10, // Replace with the number of rectangles you want
+            itemBuilder: (context, index) => Container(
+              width: 130,
+              color: Colors.blue, // Replace with the color you want
+            ),
+            separatorBuilder: (context, index) => const SizedBox(width: 20),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SpecialSection extends StatelessWidget {
+  const SpecialSection({super.key, required this.title});
+  final String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 40, 
+            color: Color.fromRGBO(255, 195, 195, 1),
+          ),
+        ),
+        SizedBox(
+          height: 150,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10, // Replace with the number of rectangles you want
+            itemBuilder: (context, index) => Container(
+              width: 130,
+              color: Colors.blue, // Replace with the color you want
+            ),
+            separatorBuilder: (context, index) => const SizedBox(width: 20),
+          ),
         ),
       ],
     );
